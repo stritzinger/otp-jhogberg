bjorng/otp

compiler: Add `update_record` instruction.
This instruction updates several tuple fields at once, improving
on the old `setelement/3 + set_tuple_element` method in the
following ways:

* Eliminates BIF call overhead, the tuple is copied and updated
  inline.
* Doesn't require special treatment in the validator.
* Uses heap allocations in the same manner as `put_tuple2` and
  friends.
* When all fields are updated with the same value (runtime check),
  we can set `Dst=Src` and move on with our day. This is impossible
  with `set_tuple_element` as it requires a new copy every time
  to avoid creating illegal references from the old heap to the
  new heap.

erts/emulator/beam/jit/arm/instr_common.cpp
@@ -640,6 +640,156 @@ void BeamModuleAssembler::emit_self(const ArgRegister &Dst) {
     mov_arg(Dst, arm::Mem(c_p, offsetof(Process, common.id)));
 }
 
+void BeamModuleAssembler::emit_copy_words_increment(arm::Gp from,
+                                                    arm::Gp to,
+                                                    size_t count) {
+    check_pending_stubs();
+
+    /* Copy the words inline if we can, otherwise use a loop with the largest
+     * vector size we're capable of. */
+    if (count <= 16) {
+        while (count >= 4) {
+            a.ldp(a64::q30, a64::q31, arm::Mem(from).post(sizeof(UWord[4])));
+            a.stp(a64::q30, a64::q31, arm::Mem(to).post(sizeof(UWord[4])));
+            count -= 4;
+        }
+    } else {
+        Label copy_next = a.newLabel();
+
+        ASSERT(Support::isUInt16(count / 4));
+        mov_imm(SUPER_TMP, count / 4);
+        a.bind(copy_next);
+        {
+            a.ldp(a64::q30, a64::q31, arm::Mem(from).post(sizeof(UWord[4])));
+            a.stp(a64::q30, a64::q31, arm::Mem(to).post(sizeof(UWord[4])));
+            a.subs(SUPER_TMP, SUPER_TMP, imm(1));
+            a.b_ne(copy_next);
+        }
+
+        count = count % 4;
+    }
+
+    if (count >= 2) {
+        a.ldr(a64::q30, arm::Mem(from).post(sizeof(UWord[2])));
+        a.str(a64::q30, arm::Mem(to).post(sizeof(UWord[2])));
+        count -= 2;
+    }
+
+    if (count == 1) {
+        a.ldr(SUPER_TMP, arm::Mem(from).post(sizeof(UWord)));
+        a.str(SUPER_TMP, arm::Mem(to).post(sizeof(UWord)));
+        count -= 1;
+    }
+
+    ASSERT(count == 0);
+    (void)count;
+}
+
+void BeamModuleAssembler::emit_update_record(const ArgWord &TupleSize,
+                                             const ArgSource &Src,
+                                             const ArgRegister &Dst,
+                                             const ArgWord &UpdateCount,
+                                             const Span<ArgVal> &updates) {
+    const size_t size_on_heap = TupleSize.get() + 1;
+    Label next = a.newLabel();
+
+    ASSERT(UpdateCount.get() == updates.size());
+    ASSERT((UpdateCount.get() % 2) == 0);
+
+    ASSERT(size_on_heap > 2);
+
+    auto destination = init_destination(Dst, ARG1);
+    auto src = load_source(Src, ARG2);
+
+    arm::Gp untagged_src = ARG3;
+    emit_untag_ptr(untagged_src, src.reg);
+
+    /* Setting a field to the same value is pretty common, so we'll check for
+     * that since it's vastly cheaper than copying if we're right, and doesn't
+     * cost much if we're wrong. */
+    if (updates.size() == 2) {
+        const auto next_index = updates[0].as<ArgWord>().get();
+        const auto &next_value = updates[1].as<ArgSource>();
+
+        safe_ldr(TMP1, arm::Mem(untagged_src, next_index * sizeof(Eterm)));
+        cmp_arg(TMP1, next_value);
+
+        if (destination.reg != src.reg) {
+            a.csel(destination.reg,
+                   destination.reg,
+                   src.reg,
+                   imm(arm::CondCode::kNE));
+        }
+        a.b_eq(next);
+    }
+
+    size_t copy_index = 0;
+
+    for (size_t i = 0; i < updates.size(); i += 2) {
+        const auto next_index = updates[i].as<ArgWord>().get();
+        const auto &next_value = updates[i + 1].as<ArgSource>();
+        bool odd_copy;
+
+        ASSERT(next_index > 0 && next_index >= copy_index);
+
+        /* If we need to copy an odd number of elements, we'll do the last one
+         * ourselves to save us from having to increment `untagged_src`
+         * separately. */
+        odd_copy = (next_index - copy_index) & 1;
+        emit_copy_words_increment(untagged_src,
+                                  HTOP,
+                                  (next_index - copy_index) & ~1);
+
+        if ((i + 2) < updates.size()) {
+            const auto adjacent_index = updates[i + 2].as<ArgWord>().get();
+            const auto &adjacent_value = updates[i + 3].as<ArgSource>();
+
+            if (adjacent_index == next_index + 1) {
+                auto [first, second] =
+                        load_sources(next_value, TMP1, adjacent_value, TMP2);
+
+                if (odd_copy) {
+                    a.ldr(TMP3, arm::Mem(untagged_src).post(sizeof(Eterm[3])));
+                    a.stp(TMP3,
+                          first.reg,
+                          arm::Mem(HTOP).post(sizeof(Eterm[2])));
+                    a.str(second.reg, arm::Mem(HTOP).post(sizeof(Eterm)));
+                } else {
+                    a.add(untagged_src, untagged_src, imm(sizeof(Eterm[2])));
+                    a.stp(first.reg,
+                          second.reg,
+                          arm::Mem(HTOP).post(sizeof(Eterm[2])));
+                }
+
+                copy_index = next_index + 2;
+                i += 2;
+                continue;
+            }
+        }
+
+        auto value = load_source(next_value, TMP1);
+
+        if ((next_index - copy_index) & 1) {
+            a.ldr(TMP2, arm::Mem(untagged_src).post(sizeof(Eterm[2])));
+            a.stp(TMP2, value.reg, arm::Mem(HTOP).post(sizeof(Eterm[2])));
+        } else {
+            a.add(untagged_src, untagged_src, imm(sizeof(Eterm)));
+            a.str(value.reg, arm::Mem(HTOP).post(sizeof(Eterm)));
+        }
+
+        copy_index = next_index + 1;
+    }
+
+    emit_copy_words_increment(untagged_src, HTOP, size_on_heap - copy_index);
+
+    sub(destination.reg,
+        HTOP,
+        (size_on_heap * sizeof(Eterm)) - TAG_PRIMARY_BOXED);
+
+    a.bind(next);
+    flush_var(destination);
+}
+
 void BeamModuleAssembler::emit_set_tuple_element(const ArgSource &Element,
                                                  const ArgRegister &Tuple,
                                                  const ArgWord &Offset) {

erts/emulator/beam/jit/x86/instr_common.cpp
@@ -770,6 +770,69 @@ void BeamModuleAssembler::emit_self(const ArgRegister &Dst) {
     mov_arg(Dst, ARG1);
 }
 
+void BeamModuleAssembler::emit_update_record(const ArgWord &TupleSize,
+                                             const ArgSource &Src,
+                                             const ArgRegister &Dst,
+                                             const ArgWord &UpdateCount,
+                                             const Span<ArgVal> &updates) {
+    size_t copy_index = 0, size_on_heap = TupleSize.get() + 1;
+    Label next = a.newLabel();
+
+    x86::Gp ptr_val;
+
+    ASSERT(UpdateCount.get() == updates.size());
+    ASSERT((UpdateCount.get() % 2) == 0);
+
+    ASSERT(size_on_heap > 2);
+
+    mov_arg(RET, Src);
+
+    /* Setting a field to the same value is pretty common, so we'll check for
+     * that since it's vastly cheaper than copying if we're right, and doesn't
+     * cost much if we're wrong. */
+    if (updates.size() == 2) {
+        const auto next_index = updates[0].as<ArgWord>().get();
+        const auto &next_value = updates[1].as<ArgSource>();
+
+        a.mov(ARG1, RET);
+        ptr_val = emit_ptr_val(ARG1, ARG1);
+        cmp_arg(emit_boxed_val(ptr_val, next_index * sizeof(Eterm)),
+                next_value,
+                ARG2);
+        a.je(next);
+    }
+
+    ptr_val = emit_ptr_val(RET, RET);
+
+    for (size_t i = 0; i < updates.size(); i += 2) {
+        const auto next_index = updates[i].as<ArgWord>().get();
+        const auto &next_value = updates[i + 1].as<ArgSource>();
+
+        ASSERT(next_index > 0 && next_index >= copy_index);
+
+        emit_copy_words(emit_boxed_val(ptr_val, copy_index * sizeof(Eterm)),
+                        x86::qword_ptr(HTOP, copy_index * sizeof(Eterm)),
+                        next_index - copy_index,
+                        ARG1);
+
+        mov_arg(x86::qword_ptr(HTOP, next_index * sizeof(Eterm)),
+                next_value,
+                ARG1);
+        copy_index = next_index + 1;
+    }
+
+    emit_copy_words(emit_boxed_val(ptr_val, copy_index * sizeof(Eterm)),
+                    x86::qword_ptr(HTOP, copy_index * sizeof(Eterm)),
+                    size_on_heap - copy_index,
+                    ARG1);
+
+    a.lea(RET, x86::qword_ptr(HTOP, TAG_PRIMARY_BOXED));
+    a.add(HTOP, imm(size_on_heap * sizeof(Eterm)));
+
+    a.bind(next);
+    mov_arg(Dst, RET);
+}
+
 void BeamModuleAssembler::emit_set_tuple_element(const ArgSource &Element,
                                                  const ArgRegister &Tuple,
                                                  const ArgWord &Offset) {

lib/compiler/src/beam_disasm.erl
@@ -412,6 +412,8 @@ disasm_instr(B, Bs, Atoms, Literals, Types) ->
 	    disasm_init_yregs(Bs, Atoms, Literals, Types);
 	bs_create_bin ->
 	    disasm_bs_create_bin(Bs, Atoms, Literals, Types);
+	update_record ->
+	    disasm_update_record(Bs, Atoms, Literals, Types);
 	_ ->
 	    try decode_n_args(Arity, Bs, Atoms, Literals, Types) of
 		{Args, RestBs} ->
@@ -487,6 +489,16 @@ disasm_bs_create_bin(Bs0, Atoms, Literals, Types) ->
     {u, Len} = U,
     {List, RestBs} = decode_n_args(Len, Bs7, Atoms, Literals, Types),
     {{bs_create_bin, [{A1,A2,A3,A4,A5,Z,U,List}]}, RestBs}.
+
+disasm_update_record(Bs1, Atoms, Literals, Types) ->
+    {Size, Bs2} = decode_arg(Bs1, Atoms, Literals, Types),
+    {Src, Bs3} = decode_arg(Bs2, Atoms, Literals, Types),
+    {Dst, Bs4} = decode_arg(Bs3, Atoms, Literals, Types),
+    {Z, Bs5} = decode_arg(Bs4, Atoms, Literals, Types),
+    {U, Bs6} = decode_arg(Bs5, Atoms, Literals, Types),
+    {u, Len} = U,
+    {List, RestBs} = decode_n_args(Len, Bs6, Atoms, Literals, Types),
+    {{update_record, [Size,Src,Dst,{{Z,U,List}}]}, RestBs}.
 
 %%-----------------------------------------------------------------------
 %% decode_arg([Byte]) -> {Arg, [Byte]}
@@ -1239,6 +1251,13 @@ resolve_inst({nif_start,[]},_,_,_) ->
     nif_start;
 resolve_inst({badrecord,[Arg]},_,_,_) ->
     {badrecord,resolve_arg(Arg)};
+
+%%
+%% OTP 26.
+%%
+
+resolve_inst({update_record,[Size,Src,Dst,List]},_,_,_) ->
+    {update_record,Size,Src,Dst,List};
 
 %%
 %% Catches instructions that are not yet handled.

stritzinger/otp-jhogberg

erts/emulator/beam/jit/arm/instr_common.cpp
@@ -640,6 +640,157 @@ void BeamModuleAssembler::emit_self(const ArgRegister &Dst) {
     mov_arg(Dst, arm::Mem(c_p, offsetof(Process, common.id)));
 }
 
+void BeamModuleAssembler::emit_copy_words_increment(arm::Gp from,
+                                                    arm::Gp to,
+                                                    size_t count) {
+    check_pending_stubs();
+
+    /* Copy the words inline if we can, otherwise use a loop with the largest
+     * vector size we're capable of. */
+    if (count <= 16) {
+        while (count >= 4) {
+            a.ldp(a64::q30, a64::q31, arm::Mem(from).post(sizeof(UWord[4])));
+            a.stp(a64::q30, a64::q31, arm::Mem(to).post(sizeof(UWord[4])));
+            count -= 4;
+        }
+    } else {
+        Label copy_next = a.newLabel();
+
+        ASSERT(Support::isUInt16(count / 4));
+        mov_imm(SUPER_TMP, count / 4);
+        a.bind(copy_next);
+        {
+            a.ldp(a64::q30, a64::q31, arm::Mem(from).post(sizeof(UWord[4])));
+            a.stp(a64::q30, a64::q31, arm::Mem(to).post(sizeof(UWord[4])));
+            a.subs(SUPER_TMP, SUPER_TMP, imm(1));
+            a.b_ne(copy_next);
+        }
+
+        count = count % 4;
+    }
+
+    if (count >= 2) {
+        a.ldr(a64::q30, arm::Mem(from).post(sizeof(UWord[2])));
+        a.str(a64::q30, arm::Mem(to).post(sizeof(UWord[2])));
+        count -= 2;
+    }
+
+    if (count == 1) {
+        a.ldr(SUPER_TMP, arm::Mem(from).post(sizeof(UWord)));
+        a.str(SUPER_TMP, arm::Mem(to).post(sizeof(UWord)));
+        count -= 1;
+    }
+
+    ASSERT(count == 0);
+    (void)count;
+}
+
+void BeamModuleAssembler::emit_update_record(const ArgAtom &Hint,
+                                             const ArgWord &TupleSize,
+                                             const ArgSource &Src,
+                                             const ArgRegister &Dst,
+                                             const ArgWord &UpdateCount,
+                                             const Span<ArgVal> &updates) {
+    const size_t size_on_heap = TupleSize.get() + 1;
+    Label next = a.newLabel();
+
+    ASSERT(UpdateCount.get() == updates.size());
+    ASSERT((UpdateCount.get() % 2) == 0);
+
+    ASSERT(size_on_heap > 2);
+
+    auto destination = init_destination(Dst, ARG1);
+    auto src = load_source(Src, ARG2);
+
+    arm::Gp untagged_src = ARG3;
+    emit_untag_ptr(untagged_src, src.reg);
+
+    /* Setting a field to the same value is pretty common, so we'll check for
+     * that since it's vastly cheaper than copying if we're right, and doesn't
+     * cost much if we're wrong. */
+    if (Hint.get() == am_reuse && updates.size() == 2) {
+        const auto next_index = updates[0].as<ArgWord>().get();
+        const auto &next_value = updates[1].as<ArgSource>();
+
+        safe_ldr(TMP1, arm::Mem(untagged_src, next_index * sizeof(Eterm)));
+        cmp_arg(TMP1, next_value);
+
+        if (destination.reg != src.reg) {
+            a.csel(destination.reg,
+                   destination.reg,
+                   src.reg,
+                   imm(arm::CondCode::kNE));
+        }
+        a.b_eq(next);
+    }
+
+    size_t copy_index = 0;
+
+    for (size_t i = 0; i < updates.size(); i += 2) {
+        const auto next_index = updates[i].as<ArgWord>().get();
+        const auto &next_value = updates[i + 1].as<ArgSource>();
+        bool odd_copy;
+
+        ASSERT(next_index > 0 && next_index >= copy_index);
+
+        /* If we need to copy an odd number of elements, we'll do the last one
+         * ourselves to save us from having to increment `untagged_src`
+         * separately. */
+        odd_copy = (next_index - copy_index) & 1;
+        emit_copy_words_increment(untagged_src,
+                                  HTOP,
+                                  (next_index - copy_index) & ~1);
+
+        if ((i + 2) < updates.size()) {
+            const auto adjacent_index = updates[i + 2].as<ArgWord>().get();
+            const auto &adjacent_value = updates[i + 3].as<ArgSource>();
+
+            if (adjacent_index == next_index + 1) {
+                auto [first, second] =
+                        load_sources(next_value, TMP1, adjacent_value, TMP2);
+
+                if (odd_copy) {
+                    a.ldr(TMP3, arm::Mem(untagged_src).post(sizeof(Eterm[3])));
+                    a.stp(TMP3,
+                          first.reg,
+                          arm::Mem(HTOP).post(sizeof(Eterm[2])));
+                    a.str(second.reg, arm::Mem(HTOP).post(sizeof(Eterm)));
+                } else {
+                    a.add(untagged_src, untagged_src, imm(sizeof(Eterm[2])));
+                    a.stp(first.reg,
+                          second.reg,
+                          arm::Mem(HTOP).post(sizeof(Eterm[2])));
+                }
+
+                copy_index = next_index + 2;
+                i += 2;
+                continue;
+            }
+        }
+
+        auto value = load_source(next_value, TMP1);
+
+        if ((next_index - copy_index) & 1) {
+            a.ldr(TMP2, arm::Mem(untagged_src).post(sizeof(Eterm[2])));
+            a.stp(TMP2, value.reg, arm::Mem(HTOP).post(sizeof(Eterm[2])));
+        } else {
+            a.add(untagged_src, untagged_src, imm(sizeof(Eterm)));
+            a.str(value.reg, arm::Mem(HTOP).post(sizeof(Eterm)));
+        }
+
+        copy_index = next_index + 1;
+    }
+
+    emit_copy_words_increment(untagged_src, HTOP, size_on_heap - copy_index);
+
+    sub(destination.reg,
+        HTOP,
+        (size_on_heap * sizeof(Eterm)) - TAG_PRIMARY_BOXED);
+
+    a.bind(next);
+    flush_var(destination);
+}
+
 void BeamModuleAssembler::emit_set_tuple_element(const ArgSource &Element,
                                                  const ArgRegister &Tuple,
                                                  const ArgWord &Offset) {

erts/emulator/beam/jit/x86/instr_common.cpp
@@ -770,6 +770,70 @@ void BeamModuleAssembler::emit_self(const ArgRegister &Dst) {
     mov_arg(Dst, ARG1);
 }
 
+void BeamModuleAssembler::emit_update_record(const ArgAtom &Hint,
+                                             const ArgWord &TupleSize,
+                                             const ArgSource &Src,
+                                             const ArgRegister &Dst,
+                                             const ArgWord &UpdateCount,
+                                             const Span<ArgVal> &updates) {
+    size_t copy_index = 0, size_on_heap = TupleSize.get() + 1;
+    Label next = a.newLabel();
+
+    x86::Gp ptr_val;
+
+    ASSERT(UpdateCount.get() == updates.size());
+    ASSERT((UpdateCount.get() % 2) == 0);
+
+    ASSERT(size_on_heap > 2);
+
+    mov_arg(RET, Src);
+
+    /* Setting a field to the same value is pretty common, so we'll check for
+     * that since it's vastly cheaper than copying if we're right, and doesn't
+     * cost much if we're wrong. */
+    if (Hint.get() == am_reuse && updates.size() == 2) {
+        const auto next_index = updates[0].as<ArgWord>().get();
+        const auto &next_value = updates[1].as<ArgSource>();
+
+        a.mov(ARG1, RET);
+        ptr_val = emit_ptr_val(ARG1, ARG1);
+        cmp_arg(emit_boxed_val(ptr_val, next_index * sizeof(Eterm)),
+                next_value,
+                ARG2);
+        a.je(next);
+    }
+
+    ptr_val = emit_ptr_val(RET, RET);
+
+    for (size_t i = 0; i < updates.size(); i += 2) {
+        const auto next_index = updates[i].as<ArgWord>().get();
+        const auto &next_value = updates[i + 1].as<ArgSource>();
+
+        ASSERT(next_index > 0 && next_index >= copy_index);
+
+        emit_copy_words(emit_boxed_val(ptr_val, copy_index * sizeof(Eterm)),
+                        x86::qword_ptr(HTOP, copy_index * sizeof(Eterm)),
+                        next_index - copy_index,
+                        ARG1);
+
+        mov_arg(x86::qword_ptr(HTOP, next_index * sizeof(Eterm)),
+                next_value,
+                ARG1);
+        copy_index = next_index + 1;
+    }
+
+    emit_copy_words(emit_boxed_val(ptr_val, copy_index * sizeof(Eterm)),
+                    x86::qword_ptr(HTOP, copy_index * sizeof(Eterm)),
+                    size_on_heap - copy_index,
+                    ARG1);
+
+    a.lea(RET, x86::qword_ptr(HTOP, TAG_PRIMARY_BOXED));
+    a.add(HTOP, imm(size_on_heap * sizeof(Eterm)));
+
+    a.bind(next);
+    mov_arg(Dst, RET);
+}
+
 void BeamModuleAssembler::emit_set_tuple_element(const ArgSource &Element,
                                                  const ArgRegister &Tuple,
                                                  const ArgWord &Offset) {

lib/compiler/src/beam_disasm.erl
@@ -412,6 +412,8 @@ disasm_instr(B, Bs, Atoms, Literals, Types) ->
 	    disasm_init_yregs(Bs, Atoms, Literals, Types);
 	bs_create_bin ->
 	    disasm_bs_create_bin(Bs, Atoms, Literals, Types);
+	update_record ->
+	    disasm_update_record(Bs, Atoms, Literals, Types);
 	_ ->
 	    try decode_n_args(Arity, Bs, Atoms, Literals, Types) of
 		{Args, RestBs} ->
@@ -487,6 +489,17 @@ disasm_bs_create_bin(Bs0, Atoms, Literals, Types) ->
     {u, Len} = U,
     {List, RestBs} = decode_n_args(Len, Bs7, Atoms, Literals, Types),
     {{bs_create_bin, [{A1,A2,A3,A4,A5,Z,U,List}]}, RestBs}.
+
+disasm_update_record(Bs1, Atoms, Literals, Types) ->
+    {Hint, Bs2} = decode_arg(Bs1, Atoms, Literals, Types),
+    {Size, Bs3} = decode_arg(Bs2, Atoms, Literals, Types),
+    {Src, Bs4} = decode_arg(Bs3, Atoms, Literals, Types),
+    {Dst, Bs6} = decode_arg(Bs4, Atoms, Literals, Types),
+    {Z, Bs7} = decode_arg(Bs6, Atoms, Literals, Types),
+    {U, Bs8} = decode_arg(Bs7, Atoms, Literals, Types),
+    {u, Len} = U,
+    {List, RestBs} = decode_n_args(Len, Bs8, Atoms, Literals, Types),
+    {{update_record, [Hint,Size,Src,Dst,{{Z,U,List}}]}, RestBs}.
 
 %%-----------------------------------------------------------------------
 %% decode_arg([Byte]) -> {Arg, [Byte]}
@@ -1239,6 +1252,13 @@ resolve_inst({nif_start,[]},_,_,_) ->
     nif_start;
 resolve_inst({badrecord,[Arg]},_,_,_) ->
     {badrecord,resolve_arg(Arg)};
+
+%%
+%% OTP 26.
+%%
+
+resolve_inst({update_record,[Hint,Size,Src,Dst,List]},_,_,_) ->
+    {update_record,Hint,Size,Src,Dst,List};
 
 %%
 %% Catches instructions that are not yet handled.